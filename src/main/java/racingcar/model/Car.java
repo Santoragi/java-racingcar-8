c216c37c5d18b0eb0e959e69995f1da2784f63ea
Santoragi <woojj1254577@naver.com>
2025-10-26 14:53:24 +0900

    feat(Car): 자동차 전진 기능
    - move(): 자동차가 전진할 수 있는지 확인 후 전진
    - moveForward(): 자동차의 position을 증가
    - isMovable(): 자동차가 전진할 수 있는지 검사

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,29 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private static final int MOVE_THRESHOLD = 4;
+
+    private String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void move(int random) {
+        if(isMovable(random)) {
+            moveForward();
+        }
+    }
+
+    private void moveForward() {
+        position++;
+    }
+
+    private boolean isMovable(int random) {
+        return random >= MOVE_THRESHOLD;
+    }
+}
