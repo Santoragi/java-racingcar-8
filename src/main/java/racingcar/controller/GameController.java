7eda05c41f8ca74522f901195297c3a48ac2e413
Santoragi <woojj1254577@naver.com>
2025-10-26 15:55:49 +0900

    feat(GameController): 우승자 추출 기능
    GameController: 자동차 리스트에서 position이 가장 높은 자동차를 추출하는 기능 추가
    - getWinners(): 주어진 자동차 리스트에서 가장 position이 높은 자동차 리스트를 반환

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -26,6 +26,9 @@ public class GameController {
         for(int i = 0; i < tryCount; i++) {
             moveAll(cars);
         }
+
+        List<Car> winners = getWinners(cars);
+
     }
 
     public List<Car> createCars(List<String> carNames) {
@@ -39,4 +42,16 @@ public class GameController {
             car.move(numberGenerator.generate());
         }
     }
+
+    public List<Car> getWinners(List<Car> cars) {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+    }
+
 }
