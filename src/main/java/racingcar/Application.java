695ef7bf5ebd2b7d37d35192ce18b5558a1d8e31
Santoragi <woojj1254577@naver.com>
2025-10-26 17:48:35 +0900

    feat(Application): 게임 실행
    GameController를 생성하고 자동차 경주를 실행하는 부분을 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,18 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.model.generator.MoveNumberGenerator;
+import racingcar.view.ConsoleInputView;
+import racingcar.view.ConsoleOutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        GameController gameController = new GameController(
+                new ConsoleInputView(),
+                new ConsoleOutputView(),
+                new MoveNumberGenerator()
+        );
+        gameController.runRacingGame();
     }
 }
